7cfd56d318e08998295fe289c9bd6f1a9987d087
LizaMarie <lizamariejimenez@gmail.com>
2017-06-28 11:34:18 -0500

    everything up do date with right code

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-const app ={
-    init(Selectors){
-        this.flick = []//empty array and everyt time submit add flick tot array
-        this.max = 0
-        this.list = document.querySelector(Selectors.listSelector)
+const app = {
+  init(selectors) {
+    this.flicks = []
+    this.max = 0
+    this.list = document.querySelector(selectors.listSelector)
 
-        document
-            .querySelector(Selectors.formSelector)
-            .addEventListener(
-                'submit', 
-                this.handleSubmit.bind(this))
-    },
-    renderListItem(flick){
-        const item = document.createElement('li')
-        item.textContent = flick.namr
-        return item
-    },
+    document
+      .querySelector(selectors.formSelector)
+      .addEventListener(
+        'submit', 
+        this.handleSubmit.bind(this)
+      )
+  },
 
-    handleSubmit(ev){
-        ev.preventDefault()
-        const flickName = ev.target.flickName.value
-        
-        const f= ev.target
-        const flick ={
-            id: this.max + 1,
-            name: f.flickName.value,
-        }
-        const listItem = this.renderListItem(flick)
-        this.list.appendChild(listItem)
-        this.max ++
-    },
+  renderListItem(flick) {
+    const item = document.createElement('li')
+    item.textContent = flick.name
+    return item
+  },
+
+  handleSubmit(ev) {
+    ev.preventDefault()
+    const f = ev.target
+    const flick = {
+      id: this.max + 1,
+      name: f.flickName.value,
+    }
+
+    const listItem = this.renderListItem(flick)
+    this.list.appendChild(listItem)
+
+    this.max ++
+  },
 }
 
-
 app.init({
-    formSelector:'form#flick-form',
-    listSelector:'#flick-list',
+  formSelector: 'form#flick-form',
+  listSelector: '#flick-list',
 })
