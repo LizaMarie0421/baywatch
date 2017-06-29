a88dcb204623e8d0496a148e29457c23d0037482
LizaMarie <lizamariejimenez@gmail.com>
2017-06-29 13:49:06 -0500

    added up and down button allowed it to change array as well as made content editable

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = {
     this.max = 0
     this.list = document.querySelector(selectors.listSelector)
     this.template = document.querySelector(selectors.templateSelector)
-    this.button= 
+
     document
       .querySelector(selectors.formSelector)
       .addEventListener(
@@ -12,63 +12,101 @@ const app = {
         this.handleSubmit.bind(this)
       )
   },
-  renderListItem(flick) {
-    const item =this.template.cloneNode(true)
-    item.classList.remove('template')
-    item.dataset.id= flick.id   
-     //allows for each new flick to have own attribute
-    item
-        .querySelector('.flick-name')
-        .textContent = flick.name
-     
-        // favButton = document.createElement("button")
-        // favButton.textContent= "I Like!!"
-        // favButton.setAttribute("class","success favButton")
-        // favButton.addEventListener('click',this.changeFavButton.bind(this))
-        // item.appendChild(favButton)
-    item
-        .querySelector('button.remove')
-        .addEventListener(
-            'click',
-            'this.removeFlick').bind(this,flick)
-    item
-        .querySelector('button.fav')
-        .addEventListener(
-            'click',
-            'this.favFlick').bind(this,flick)            
-        // delButton = document.createElement("button")
-        // delButton.textContent= "Delete Please"
-        // delButton.setAttribute("class","success delButton")
-        // delButton.addEventListener('click',this.DeleteButton.bind(this))
-        // item.appendChild(delButton)
-        return item
+  moveDownFlick(flick, ev) {
+      const listItem = ev.target.closest('.flick')
+      this.list.insertBefore(listItem, listItem.nextSibling.nextSibling)
+
+    const currentflick = this.flicks[i]
+    const prevflick = this.flicks[i+1]
+    this.flicks[i] = prevflick
+    this.flicks[i+1] = currentflick
   },
+  moveUpFlick(flick, ev) {
+    //added in dom
+      const listItem = ev.target.closest('.flick')
+      this.list.insertBefore(listItem, listItem.previousSibling)
+    //add an array 
+    const i = this.flicks.indexOf(flick)
+
+    const currentflick = this.flicks[i]
+    const prevflick = this.flicks[i-1]
+    this.flicks[i] = prevflick
+    this.flicks[i-1] = currentflick
+  },
+  favFlick(flick, ev) {
+    const listItem = ev.target.closest('.flick')
+    flick.fav = !flick.fav
+
+    if (flick.fav) {
+      listItem.classList.add('fav')
+    } else {
+      listItem.classList.remove('fav')
+    }
+  },
+
+  removeFlick(flick, ev) {
+    // remove from the DOM
+    const listItem = ev.target.closest('.flick')
+    listItem.remove()
+
+    // remove from the array
+    const i = this.flicks.indexOf(flick)
+    this.flicks.splice(i, 1)
+  },
+
+  renderListItem(flick) {
+    const item = this.template.cloneNode(true)
+    item.classList.remove('template')
+    item.dataset.id = flick.id
+    item
+      .querySelector('.flick-name')
+      .textContent = flick.name
+
+    item
+      .querySelector('button.remove')
+      .addEventListener(
+        'click', 
+        this.removeFlick.bind(this, flick)
+      )
+
+    item
+      .querySelector('button.fav')
+      .addEventListener(
+        'click', 
+        this.favFlick.bind(this, flick)
+      )
+    item
+      .querySelector('button.up')
+      .addEventListener(
+        'click', 
+        this.moveUpFlick.bind(this, flick)
+      )  
+    item
+      .querySelector('button.down')
+      .addEventListener(
+        'click', 
+        this.moveDownFlick.bind(this, flick)
+      )
+    return item
+  },
+
   handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const flick = {
       id: this.max + 1,
       name: f.flickName.value,
-      favStatus: false,
+      fav: false,
     }
-    const listItem = this.renderListItem(flick)
-    this.list.insertBefore(listItem, this.list.firstElementChild)
-    this.flicks.unshift(flick)
-    this.max ++
-    f.reset() 
-  },
-  favFlck(flick, ev){
-    const listItem= ev.target.closest('.flick')
-    listItem.classList.add('fav')
-    flick.fav=true
 
-  },
-  removeFlick(flick, ev){
-     //remove from dom
-     ev.target.closest('.flick').remove()
-     //remove from array
-    const i = this.flicks.indexOf(flick)
-    this.flicks.splice(i,1)
+    this.flicks.unshift(flick)
+
+    const listItem = this.renderListItem(flick)
+    this.list
+      .insertBefore(listItem, this.list.firstElementChild)
+
+    this.max ++
+    f.reset()
   },
 }
 
